91193f00d40dea77b0a8b89f3d20e1752de763ae
Sam Von Ehren <sam.von.ehren@gmail.com>
2024-10-10 23:28:48 -0400

    Adding in nodemon, writing functions for finding words based on green and yellow words

--- wordlist.js
+++ wordlist.js
@@ -5,6 +5,31 @@ var words = [];
 
 words.push("SALET", "BALET", "PARTY");
 
+function FindWordsForGreenLetters(currentGreenLetters){
+    return words.filter((word) => {
+        for(var i = 0; i < currentGreenLetters.length; i++) {
+            if(currentGreenLetters[i] !== "_" && currentGreenLetters[i] !== word[i]) {
+                return false;
+            }
+        }
+        return true;
+    });
+}
+
+function FindWordsForYellowLetters(currentFilteredList, currentYellowLetters) {
+    return currentFilteredList.filter((word) => {
+        // TODO probably will need to make this handle double letters
+        for(var i = 0; i < currentYellowLetters.length; i++) {
+            var letter = currentYellowLetters[i];
+            if(letter === "_") continue;
+            var firstAppearance = word.indexOf(letter);
+            if(firstAppearance < 0) return false;
+            if(firstAppearance == i && word.lastIndexOf(letter) == i) return false; 
+        }
+        return true;
+    });
+}
+
 moveChecker.WordExists = function(word) {
     for(var i = 0; i < words.length; i++) {
         if(words[i] == word) return true;
@@ -13,7 +38,9 @@ moveChecker.WordExists = function(word) {
 }
 
 moveChecker.wordsRemaining = function(word, ruleObject) {
-    return this.WordExists(word);
+    var filteredForGreen = FindWordsForGreenLetters("___ET");
+    var filteredForYellow = FindWordsForYellowLetters(filteredForGreen, "_S___");
+    return filteredForYellow.length > 0;
 }
 
 moveChecker.words = words;
